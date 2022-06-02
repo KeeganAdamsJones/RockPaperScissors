2f49902624e32f95aa3214e1c9266ce2fde9ade8
Keegan Adams Jones <73457258+KeeganAdamsJones@users.noreply.github.com>
2022-06-02 16:22:42 -0500

    Update rockPaperScissors.java

--- rockPaperScissors.java
+++ rockPaperScissors.java
@@ -1,76 +1,114 @@
-/*
+package rockpaperscissors;
+
+/**
+ /*
 Keegan Jones
-1/18/2021
+6/1/2022
 Rock Paper Scissors
 
 
 
-Write a program that is similar to the popular game titled “Rock-Paper-Scissors.”
+A program that is similar to the popular game titled “Rock-Paper-Scissors.”
 The program randomly generates the number of 1, 2, or 3 with 1 being Rock, 2 
 being Paper, and 3 being Scissors. Then, the program is to prompt the user to 
-enter a value of 1, 2, or 3. Next, the program displays a clear readable message 
+enter a value of R, P, or S. Next, the program displays a clear readable message 
 displaying both the computer’s selection, the user’s selection, and the results.
  */
 
-package rockpaperscissors;
 
-/**
- *
- * @author keega
- */
 import java.util.Scanner; //import scanner to recieve input
 
-public class rockPaperScissors {
+public class RockPaperScissors {
 
     public static void main(String[] args) {
-    
-        //Greet player
-        System.out.println("\n\nHello, Welcome to Rock, Paper, Scissors!\n");
+        // 1. Computer picks a random number
+        // 2. User will make selection or R,P,S or Q.
+        // 3. Program will evaluate who won.
         
-        // Prompt the user to enter 1 for rock, 2 for paper, or 3 for scissors       
+        
+        //Greet player
+        System.out.println("\n\nHello, Welcome to Rock, Paper, Scissors!\n\n");
+        
+        
         while (true){
+          
+            // Prompt the user to enter R for rock, P for paper, or S for scissors 
+            System.out.print
+                ("Please enter your selection: \n");        
+            System.out.print
+                ("\tR for rock, P for paper or S for scissors. (If you no longer wish to play select Q.)  \n\n");
+           
+
+            // New scanner object to take input selection from player
+            Scanner input = new Scanner(System.in);
+            String selection = input.next();
+
+             
+            //Validate the user input before the evaluation If structure
+            if(selection.equalsIgnoreCase("Q")) {
+               System.out.println("Thank you for playing.  Goodbye!");
+               return;
+            }
+
+            if(!selection.equalsIgnoreCase("P") &&
+               !selection.equalsIgnoreCase("S") && 
+               !selection.equalsIgnoreCase("R")){
+                System.out.println("You have made an invalid selection.");
+                continue;
+            }
+            
             // Generate random single digit integer
             int number = (int)(Math.random() * 4);
-        
-        System.out.print
-        ("Please enter your selection: 1 for rock, 2 for paper or 3 for "
-                    + "scissors.  If you no longer wish to play select 0.\n\n");
-        Scanner input = new Scanner(System.in);
-        int selection = input.nextInt();
-        
-        //display the number and selection and the resulting winner
-        if (number == 1 && selection == 1)
-            System.out.println("You both chose rock, you have a tie!\n\n");
-            else if (number == 1 && selection == 2)
-                System.out.println("The computer chose rock and you chose paper.\n"
-                        + "Paper covers rock, you win!\n\n");
-            else if (number == 1 && selection == 3)
-                System.out.println("The computer chose rock and you chose scissors.\n"
-                        + "Rock crushes scissors, you lose.\n\n");
-            else if (number == 2 && selection == 1)
-                System.out.println("The computer chose paper and you chose rock.\n"
-                        + "Paper covers rock, you lose.\n\n");
-            else if (number == 2 && selection == 2)
-                System.out.println("You both chose paper, you have a tie!\n");
-            else if (number == 2 && selection == 3)
-                System.out.println("The computer chose paper and your chose scissors.\n"
-                        + "Scissors cuts paper, you win!\n\n");
-            else if (number == 3 && selection == 1)
-                System.out.println("The computer chose scissors and you chose rock.\n"
-                        + "Rock crushes scissors, you win!\n\n");
-            else if (number == 3 && selection == 2)
-                System.out.println("The computer chose scissors and you chose paper.\n"
-                        + "Scissors cut paper, you lose.\n\n");
-            else if (number == 3 && selection == 3)
-                System.out.println("You both chose scissors, you have a tie!\n"); 
-            else if (selection == 0){
-                System.out.println("Thank you for playing! Goodbye.");
-                break;
+            
+            // ** Nested If structure to evaluate results **
+            // Computer chose Rock - (computer generated random number)
+            if (number == 1){ 
+                if(selection.equalsIgnoreCase("R")){
+                    System.out.println("\t\t\tYou both chose rock, you have a tie!\n\n");
+                }
+                else if(selection.equalsIgnoreCase("P")){
+                    System.out.println("\t\t\tThe computer chose rock and you chose paper.\n"
+                        + "\t\t\tPaper covers rock, you win!\n\n");
+                }
+                else if(selection.equalsIgnoreCase("S")){
+                    System.out.println("\t\t\tThe computer chose rock and you chose scissors.\n"
+                        + "\t\t\tRock crushes scissors, you lose.\n\n");
+                }
             }
-                                         
-                       
+            
+            // Computer chose Paper - (computer generated random number)
+            else if (number == 2){
+                if(selection.equalsIgnoreCase("R")){
+                    System.out.println("\t\t\tThe computer chose paper and you chose rock.\n"
+                        + "\t\t\tPaper covers rock, you lose.\n\n");
+                }
+                else if(selection.equalsIgnoreCase("P")){
+                    System.out.println("\t\t\tYou both chose paper, you have a tie!\n");
+                }
+                else if(selection.equalsIgnoreCase("S")){
+                    System.out.println("\t\t\tThe computer chose paper and your chose scissors.\n"
+                        + "\t\t\tScissors cuts paper, you win!\n\n");
+                }
+            }
+            
+            // Computer chose Scissors - (computer generated random number)
+            else if (number == 3){ 
+                if(selection.equalsIgnoreCase("R")){
+                    System.out.println("\t\t\tThe computer chose scissors and you chose rock.\n"
+                        + "\t\t\tRock crushes scissors, you win!\n\n");
+                }
+                else if(selection.equalsIgnoreCase("P")){
+                    System.out.println("\t\t\tThe computer chose scissors and you chose paper.\n"
+                        + "\t\t\tScissors cut paper, you lose.\n\n");
+                }
+                else if(selection.equalsIgnoreCase("S")){
+                    System.out.println("\t\t\tYou both chose scissors, you have a tie!\n"); 
+                }
+            }
+            
         }
         
     }
     
 }
+
